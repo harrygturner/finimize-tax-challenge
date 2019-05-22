e2bcd8ea483943a861bf13c76f4784545480384d
github username <github email address>
2019-05-22 14:15:15 +0100

    nav rotates

--- src/App.js
+++ src/App.js
@@ -7,11 +7,34 @@ import Navigation from './components/Navigation';
 
 class App extends Component {
 
+  componentDidMount() {
+    document.addEventListener('scroll', this.trackScrolling);
+  }
+
+  trackScrolling = () => {
+    const introEl = document.querySelector('#intro');
+    const navIcon = document.querySelector('#nav .icon i');
+    if(introEl.getBoundingClientRect().bottom < 100){
+      navIcon.classList.add('rotate')
+    } else {
+      navIcon.classList.remove('rotate')
+
+    }
+  }
+
+  animateScrollArrow = event => {
+    console.log('animate')
+  }
+
   render() {
     return (
       <div className="App">
-        < Introduction renderQuotes={this.quoteAnimation} />
-        < Navigation />
+        < Introduction 
+          renderQuotes={this.quoteAnimation} 
+        />
+        < Navigation 
+          animateScrollArrow={this.animateScrollArrow} 
+        />
         < Calculator />
       </div>
     );
