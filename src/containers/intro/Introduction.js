ed8474aaa1f06bc8e40b7fcb5cd269ed47366352
github username <github email address>
2019-05-22 12:45:50 +0100

    intro first edition done

--- src/containers/intro/Introduction.js
+++ src/containers/intro/Introduction.js
@@ -14,6 +14,21 @@ export default class Introduction extends Component {
                   <h1>INCOME TAX CALCULATOR</h1>
                   <h3>Are you paying too much income tax?</h3>
                </div>
+               <div className='content'>
+                  Confused by your income tax and National Insurance? Our calculator will not only give you an indication of what you should be paying based on your annual salary for the financial year 2019/20 but it will also work out how much you could be <span className='bold'>overpaying</span>!
+                  <br /><br />
+                  - Are you contrinbuting to a pension scheme or paying of your student loan? 
+                  <br />
+                  - Are you self-employed?
+                  <br /> 
+                  - Are you entitled to tax free allowances?
+                  <br /> 
+                  - Are you married or in a civil partnership? 
+                  <br /><br />
+                  <span className='bold'>All these factors and more can offer you some much needed tax relief.</span>
+                  <br /><br />
+                  Do not worry! We will not be making you fill out a full tax form, just follow the arrows, input your salary and let us do the magic.
+               </div>
             </div>
          </div>
       )
